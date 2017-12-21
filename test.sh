187f8ed609e2c45d757030eaf3571defbdd22887
Junjie Bai <bai@in.tum.de>
2017-12-22 07:45:07 +0800

    Run tests in parallel

--- test.sh
+++ test.sh
@@ -13,8 +13,18 @@ else
     PYTHON="python"
 fi
 
-$PYTHON "$TEST_DIR/test_operators.py" -v
-$PYTHON "$TEST_DIR/test_models.py" -v
-$PYTHON "$TEST_DIR/test_caffe2.py" -v
-$PYTHON "$TEST_DIR/test_verify.py" -v
-$PYTHON "$TEST_DIR/test_pytorch_helper.py" -v
+test_files=(
+    "$TEST_DIR/test_operators.py"
+    "$TEST_DIR/test_models.py"
+    "$TEST_DIR/test_caffe2.py"
+    "$TEST_DIR/test_verify.py"
+    "$TEST_DIR/test_pytorch_helper.py"
+)
+
+if hash parallel 2>/dev/null; then
+    parallel -j 2 -t --line-buffer --keep-order $PYTHON {} -v ::: "${test_files[@]}"
+else
+    for f in "${test_files[@]}"; do
+        $PYTHON $f -v
+    done
+fi
